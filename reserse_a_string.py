33e38765afbe6eb85d498ca0d54d7aefd43d666d
Bethwel Kiplagat <sjemnyango9@gmail.com>
2025-01-24 10:51:11 +0300

    User input added

--- reserse_a_string.py
+++ reserse_a_string.py
@@ -1,7 +1,21 @@
+
+#
+
+#  Problem 1: Reverse a String
+# Write a Python function that takes a string as input and returns the reversed version of the string.
+
+
+
+
 def reverse_string(s):
     return s[::-1]
 
 # Test the function
-input_string = "hello"
+# input_string = "hello"
+# output = reverse_string(input_string)
+# print("Reversed String:", output)
+
+# Ask the user for input
+input_string = input("Enter a string to reverse: ")
 output = reverse_string(input_string)
 print("Reversed String:", output)
